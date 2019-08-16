79b573e8e11256a0b3eb9879865fa4e3d39176c2
digitalfrost <gerald.hofmaier@gmail.com>
2019-08-16 19:23:55 +0200

    Placeholder for googleDriveUploader

---
+++ octoprint_cloud_timelapse/uploaders/googleDriveUploader.py
@@ -0,0 +1,9 @@
+'''
+    googleDriveUploader
+    Uploads files to Google Drive
+    if upload was successfull returns True
+'''
+
+def upload(self):
+    self._logger.warning("googleDriveUploader not yet implemented")
+    raise NotImplementedError
