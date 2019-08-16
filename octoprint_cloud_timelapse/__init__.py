181e7ad8e664932e40832a135e9dd3defd3293b3
digitalfrost <gerald.hofmaier@gmail.com>
2019-08-16 10:27:27 +0200

    settings for the plugin

--- octoprint_cloud_timelapse/__init__.py
+++ octoprint_cloud_timelapse/__init__.py
@@ -8,7 +8,39 @@ import os
 class CloudTimelapsePlugin(
         octoprint.plugin.SettingsPlugin,octoprint.plugin.EventHandlerPlugin,octoprint.plugin.TemplatePlugin, octoprint.plugin.RestartNeedingPlugin):
 
+    @property
+    def api_token(self):
+        return self._settings.get(['api_token'])
 
+    @property
+    def cloud_provider(self):
+        return self._settings.get(['cloud_provider'])
+
+    @property
+    def delete_after_upload(self):
+        return self._settings.get_boolean(['delete_after_upload'])
+
+    ##~~ SettingsPlugin mixin
+    
+    def get_settings_defaults(self):
+        return dict(
+            api_token = None,
+            cloud_provider = 'dropbox'
+            delete_after_upload = False
+        )
+
+    def get_settings_restricted_paths(self):
+        return dict(
+            admin=[['api_token'], ]
+        )
+
+    def get_template_configs(self):
+        return [
+            dict(type ='settings', custom_bindings = False, template ='cloud_timelapse_settings.jinja2')
+        ]
+
+    ##~~ Software update hook
+    
     def get_update_information(self):
         return dict(
             dropbox_timelapse=dict(
